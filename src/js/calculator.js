ed961c219a9695ebbeba4cd839d03af26320fb3b
Eric Hongmoungkhoune <ehmk90@gmail.com>
2021-05-11 22:57:37 -0400

    Created a serviceable user interface

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,14 +1,22 @@
 const operations = {
     add: function(a, b) {
+        currentTotal = a + b;
+        screenValue.textContent = currentTotal;
         return a + b;
     },
     subtract: function(a, b) {
+        currentTotal = a + b;
+        screenValue.textContent = currentTotal;
         return a - b;
     },
     multiply: function(a, b) {
+        currentTotal = a + b;
+        screenValue.textContent = currentTotal;
         return a * b;
     },
     divide: function(a, b) {
+        currentTotal = a + b;
+        screenValue.textContent = currentTotal;
         return a / b;
     },
     operate: function(operation, a, b) {
@@ -26,7 +34,6 @@ const operations = {
     },
 };
 
-
 const dom = {
     oneButton: document.querySelector('#one-button'),
     twoButton: document.querySelector('#two-button'),
@@ -38,10 +45,15 @@ const dom = {
     eightButton: document.querySelector('#eight-button'),
     nineButton: document.querySelector('#nine-button'),
     zeroButton: document.querySelector('#zero-button'),
+    
+    additionButton: document.querySelector('#addition-button'),
+    subtractionButton: document.querySelector('#subtraction-button'),
+    multiplicationButton: document.querySelector('#multiplication-button'),
+    divisionButton: document.querySelector('#division-button'),
+    equalityButton: document.querySelector('#equality-button'),
+    operateButton: document.querySelector('#operate-button'),
     clearButton: document.querySelector('#clear-button'),
 };
-
-let screenValue = document.querySelector('#screen-value');
 
 dom.oneButton.addEventListener('click', () => screenValue.textContent += '1');
 dom.twoButton.addEventListener('click', () => screenValue.textContent += '2');
@@ -53,7 +65,17 @@ dom.sevenButton.addEventListener('click', () => screenValue.textContent += '7');
 dom.eightButton.addEventListener('click', () => screenValue.textContent += '8');
 dom.nineButton.addEventListener('click', () => screenValue.textContent += '9');
 dom.zeroButton.addEventListener('click', () => screenValue.textContent += '0');
+
+dom.additionButton.addEventListener('click', () => screenValue.textContent = operations.add(currentTotal + screenValue.value));
+dom.subtractionButton.addEventListener('click', () => screenValue.textContent = '');
+dom.multiplicationButton.addEventListener('click', () => screenValue.textContent = '');
+dom.divisionButton.addEventListener('click', () => screenValue.textContent = '');
+dom.equalityButton.addEventListener('click', () => screenValue.textContent = '');
+dom.operateButton.addEventListener('click', () => screenValue.textContent = '');
 dom.clearButton.addEventListener('click', () => screenValue.textContent = '');
+
+let screenValue = document.querySelector('#screen-value');
+let currentTotal = 0;
 
 
 
